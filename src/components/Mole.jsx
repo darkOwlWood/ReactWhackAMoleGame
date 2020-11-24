9dee2092cbdc1ae0e4679671d75df65674776cff
Daniel Reyes <danielckings@gmail.com>
2020-11-24 13:22:40 -0600

    Get a mole type base on random probability and get the mole css class methods were added

--- src/components/Mole.jsx
+++ src/components/Mole.jsx
@@ -5,17 +5,17 @@ const IN_STATE  = 0;
 const OUT_STATE = 1;
 const HIT_STATE = 2;
 
-const NO_MOLE   = {id:-1, points: 0};
-const RED_MOLE  = {id:0,  points: 100};
-const BLUE_MOLE = {id:1,  points: 150};
-const GOLD_MOLE = {id:2,  points: 300};
+const NO_MOLE   = {id:-1, points: 0  , className: 'mole--active-zero'};
+const RED_MOLE  = {id:0,  points: 100, className: 'mole--active-red' };
+const BLUE_MOLE = {id:1,  points: 150, className: 'mole--active-blue'};
+const GOLD_MOLE = {id:2,  points: 300, className: 'mole--active-gold'};
 
 const GLOBAL_WAIT_TIME = 4000;
 const MOLE_OUT_TIME = 2000;
 const MOLE_WAIT_TIME = 1000;
 const MAX_MOLE = 8;
 
-const Mole = ({id,activeMole,molePoints}) => {
+const Mole = ({activeMole,molePoints}) => {
 
     const timerId = useRef(null);
     const [moleState, setMoleState] = useState({position: IN_STATE, type:NO_MOLE.id});
@@ -25,7 +25,7 @@ const Mole = ({id,activeMole,molePoints}) => {
             if(moleState.position===IN_STATE && activeMole.current < MAX_MOLE){
                 if(Math.floor(Math.random()*2)){//THE GOES OUT OR NOT
                     activeMole.current = activeMole.current + 1;
-                    setMoleState({position: OUT_STATE, type: Math.floor(Math.random()*3)});
+                    setMoleState({position: OUT_STATE, type: getMoleType()});
                     setTimeout(() => {
                         activeMole.current = activeMole.current - 1;
                         setMoleState({...moleState,position:IN_STATE});
@@ -39,8 +39,31 @@ const Mole = ({id,activeMole,molePoints}) => {
         },MOLE_WAIT_TIME);
     },[moleState]);
 
-    const checkMoleType = () => {
 
+    function getMoleType(){
+        let moleType = 0;
+        const randomNumber = Math.floor(Math.random()*100)+1;
+
+        moleType = (randomNumber>=1 && randomNumber<=50)?
+                    RED_MOLE.id
+                    :(randomNumber>=51 && randomNumber<=90)?
+                    BLUE_MOLE.id
+                    :(randomNumber>=91 && randomNumber<=100)?
+                    GOLD_MOLE.id : NO_MOLE.id;
+
+        return moleType;
+    }
+
+    const getMoleCssClass = (moleType) => {
+        let cssClassName = '';
+        switch(moleType){
+            case RED_MOLE.id:  cssClassName=RED_MOLE.className;  break;
+            case BLUE_MOLE.id: cssClassName=BLUE_MOLE.className; break; 
+            case GOLD_MOLE.id: cssClassName=GOLD_MOLE.className; break; 
+            default: cssClassName=NO_MOLE.className;
+        }
+
+        return cssClassName;
     }
 
     const handleClick = () =>{
@@ -51,13 +74,7 @@ const Mole = ({id,activeMole,molePoints}) => {
 
     return (
         <div 
-            className={`mole ${
-                    moleState.type===RED_MOLE.id? 
-                        'mole--active-red'
-                        :moleState.type===BLUE_MOLE.id?
-                        'mole--active-blue'
-                        :'' 
-                }`} 
+            className={`mole ${getMoleCssClass(moleState.type)}`} 
             onClick={moleState.position===OUT_STATE? handleClick : ()=>{}}
         >
 
